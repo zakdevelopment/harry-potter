42c2b197edacf492c10f9319460a573849138ab0
Daniel Żak <icobragroup@gmail.com>
2025-11-03 09:39:15 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log("✨ Witaj w magicznym świecie! Kliknij przycisk i baw się motywami ✨");
+
+const body = document.querySelector("body");
+const button = document.querySelector(".button");
+
+button.addEventListener("click", () => {
+  body.classList.toggle("lumos");
+  body.classList.toggle("nox");
+
+  button.textContent = body.classList.contains("lumos") 
+    ? "Jasny" 
+    : "Ciemny"; 
+});
