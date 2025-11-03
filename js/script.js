fb35769cf158716bade87a173be2049e52c7834a
Daniel Żak <icobragroup@gmail.com>
2025-11-03 10:09:33 +0100

    changed welcome messange in the console

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("✨ Witaj w magicznym świecie! Kliknij przycisk i baw się motywami ✨");
+console.log("✨ Witaj w magicznym świecie! Kliknij przycisk i baw się motywami ✨ Ten kod jest już w repozytorium GIT");
 
 const body = document.querySelector("body");
 const button = document.querySelector(".button");
@@ -7,7 +7,7 @@ button.addEventListener("click", () => {
   body.classList.toggle("lumos");
   body.classList.toggle("nox");
 
-  button.textContent = body.classList.contains("lumos") 
-    ? "Jasny" 
-    : "Ciemny"; 
+  button.textContent = body.classList.contains("lumos")
+    ? "Jasny"
+    : "Ciemny";
 });
